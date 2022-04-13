f28b8f6de17d45d16fabebe08a79d251b82a2d51
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2022-04-12 22:56:05 -0400

    Add files via upload

---
+++ Cryptography/basic-mod1/Basic-Mod1-Script.py
@@ -0,0 +1,21 @@
+# original script cleaned up a bit following the CTF and implemented some aspects of the following work
+# https://github.com/CavemanJay/PicoCTF/blob/master/2022/cryptography/basic_mod1/get_flag.py
+
+file = open("message.txt", "r").read().split(" ")
+message = [int(f) for f in file if f != '']
+print(message)
+
+def decrypt_message(value):
+    result = value % 37
+    if (result == 36): # Under score
+        return "_"
+    elif result in range(0,26): # Alphabet
+        return chr(result + 65)
+    elif result in range(26,36): # Decimals
+        return str(result - 26)
+    else:
+        return None
+
+flagList = [decrypt_message(m) for m in message]
+flag = ''.join(flagList)
+print(flag)
