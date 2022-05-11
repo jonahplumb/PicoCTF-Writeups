5624dcd53158d38919aac10d4a3780804e276a05
jonahplumb <98274184+jonahplumb@users.noreply.github.com>
2022-05-11 18:58:05 -0400

    Update get_flag.py

--- Cryptography/transposition-trial/get_flag.py
+++ Cryptography/transposition-trial/get_flag.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-file = open("message.txt", "r").read()
+file = open("message.txt", "r").read() # read file
 print(file) #print our message
 
 flag = ""
 for n in range(0, len(file), 3):
-    blocks = file[n: n + 3]
-    flag += blocks[2] + blocks[0:2]
+    blocks = file[n: n + 3]  # creating temporary blocks of 3
+    flag += blocks[2] + blocks[0:2] # rearranging blocks
 
 print(flag)
